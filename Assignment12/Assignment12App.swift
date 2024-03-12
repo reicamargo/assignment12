7db1a25c4868e98118e9a568419aceb7f077febb
Reinaldo Camargo <camargo.reinaldo@hotmail.com>
2024-03-12 17:06:51 -0300

    First commit

---
+++ Assignment12/Assignment12App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment12App.swift
+//  Assignment12
+//
+//  Created by Reinaldo Camargo on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment12/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment12
+//
+//  Created by Reinaldo Camargo on 12/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
